05f5ab3ed9e7890e8b84a0d784cebd7e8777b0a1
Scott Erwin <scottallenerwin@gmail.com>
2021-03-05 16:25:27 -0500

    script 2 was first attempts super failed script 1 works but only with checking for if its a number/and lenths of that number

--- js/script.js
+++ js/script.js
@@ -1,5 +1,35 @@
 // Assignment Code
+// how to generate random number with math.random() https://www.w3schools.com/js/js_random.asp
+var lowercase = "abcdefghijklmnopqrstuvwxyz"
+var uppercase = "ABCDEGHHIJKLMNOPQRSTUVWXYZ"
+var numbers = "1234567890"
+var symbols = " !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~"
+var empty = "";
+
 var generateBtn = document.querySelector("#generate");
+
+
+function generatePassword() {
+  finalpass = '';
+  let combinedImput = empty.concat(lowercase, uppercase, numbers, symbols);
+  console.log(combinedImput);
+  let combinedImputArray = combinedImput.split('');
+  console.log(combinedImputArray);
+
+  var lengthSelected = prompt("Select a numerical value with a length of at least 8 characters and no more than 128 characters", "8");
+  
+  if (isNaN(lengthSelected) || lengthSelected < 8 || lengthSelected > 128) {
+    alert("That was not a numerical Value please try again or less then 8 or greater then 128");
+  } else {
+    let pass
+    for (let i = 0; i < lengthSelected; i++) {
+      pass = combinedImputArray[Math.round(Math.random() * (combinedImputArray.length - 1))];
+      finalpass += pass;
+    }
+  }
+  return finalpass;
+
+}
 
 // Write password to the #password input
 function writePassword() {
